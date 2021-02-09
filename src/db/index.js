1acffe38151fc89402c118c4a380c8eee35dea60
justin-moretto <JustinMorettoMTL@gmail.com>
2021-02-09 18:50:35 +0000

    added reject unauthorized: false to ssl object in pg.client object

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,10 @@ const pg = require("pg");
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
-  ssl: true
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
 });
 
 client
